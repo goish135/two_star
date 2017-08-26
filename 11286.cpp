5a575c776e9f9f21f1cbeabcac7960be145acbda
阿瑜 <goish2468@gmail.com>
2017-08-25 20:02:26 -0500

    Comformity(排序)
    參考網址:http://hoyusun.blogspot.tw/2012/09/uva-11286-conformity.html?m=1

---
+++ 11286.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+//#include<string>
+#include <cstring>
+#include<map>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int fm;//freshman
+	int c[5];//class
+	char s[100];//
+	while(scanf("%d",&fm))
+	{
+		if(fm==0) break;
+		map<string,int> myMap;
+		int ss;
+		//while(fm--)
+	    for(int k = 0; k < fm; k++)
+		{
+			//cin>>c[0]>>c[1]>>c[2]>>c[3]>>c[4];//five class
+			for(int j = 0; j < 5; j++)
+                scanf("%d", &c[j]);
+			sort(c,c+5);
+			sprintf(s,"%d%d%d%d%d",c[0],c[1],c[2],c[3],c[4]);
+			myMap[s]++;
+		}//for k//O
+		//一組測資輸入完 //
+			ss = 0;//set sum
+			int max = 0;
+			//做一次性的排序 
+			for(map<string,int>::iterator i = myMap.begin();i!=myMap.end();i++)
+			{
+				if(i->second>max) 
+				{
+					max = i->second;
+					ss=0;
+				}
+				if(i->second==max)
+				{
+					ss+=max;
+				}
+			}
+			//cout<<ss<<endl;
+		//}//for k //括錯地方//X 
+		//cout<<ss<<endl;
+		printf("%d\n",ss);
+	}
+	return 0;	
+} 
